d0cf572c431150be2c194441aa4674664d4b6dce
kairith <159109160+kairith@users.noreply.github.com>
2024-11-11 15:25:50 +0700

    W4-S3

--- 1 - START CODE/EX-3/ex_3_start.dart
+++ 1 - START CODE/EX-3/ex_3_start.dart
@@ -1,36 +1,71 @@
 import 'package:flutter/material.dart';
 
 List<String> images = [
-  "assets/w4-s2/bird.jpg",
-  "assets/w4-s2/bird2.jpg",
-  "assets/w4-s2/insect.jpg",
-  "assets/w4-s2/girl.jpg",
-  "assets/w4-s2/man.jpg",
+  "assets/images/bird.jpg",
+  "assets/images/bird2.jpg",
+  "assets/images/insect.jpg",
+  "assets/images/girl.jpg",
+  "assets/images/man.jpg",
 ];
+void main() => runApp(const MyApp());
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-void main() => runApp(MaterialApp(
-      debugShowCheckedModeBanner: false, // Why this line ? Can you explain it ?
-      home: Scaffold(
-        backgroundColor: Colors.green[50],
-        appBar: AppBar(
-          backgroundColor: Colors.green[400],
-          title: const Text('Image viewer'),
-          actions: <Widget>[
-            IconButton(
-              icon: const Icon(Icons.navigate_before),
-              tooltip: 'Go to the previous image',
-              onPressed: () => {},
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false, 
+      home: ImageViewer(),
+    );
+  }
+}
+
+class ImageViewer extends StatefulWidget {
+  const ImageViewer({super.key});
+
+  @override
+  _ImageViewerState createState() => _ImageViewerState();
+}
+
+class _ImageViewerState extends State<ImageViewer> {
+  int _currentIndex = 0;
+
+  void _nextImage() {
+    setState(() {
+      _currentIndex = (_currentIndex + 1) % images.length;
+    });
+  }
+  void _previousImage() {
+    setState(() {
+      _currentIndex = (_currentIndex - 1 + images.length) % images.length;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.green[50],
+      appBar: AppBar(
+        backgroundColor: Colors.green[400],
+        title: const Text('Image Viewer'),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.navigate_before),
+            tooltip: 'Go to the previous image',
+            onPressed: _previousImage,
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0, 0, 50, 0),
+            child: IconButton(
+              icon: const Icon(Icons.navigate_next),
+              tooltip: 'Go to the next image',
+              onPressed: _nextImage, 
             ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(0, 0, 50, 0),
-              child: IconButton(
-                icon: const Icon(Icons.navigate_next),
-                tooltip: 'Go to the next image',
-                onPressed: () => {},
-              ),
-            ),
-          ],
-        ),
-        body: Image.asset(images[0]),
+          ),
+        ],
       ),
-    ));
+      body: Center(
+        child: Image.asset(images[_currentIndex]),
+      ),
+    );
+  }
+}
